c9eb71f273ef38636a3a6fd284dde20413b3f70d
John Murphy <johnmurphy@Johns-MacBook-Pro.local>
2018-01-03 23:25:41 -0800

    cron at 6:31 runs incremently for a while

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const { CronJob } = require('cron');
+
+const scrapeGainers = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://finance.yahoo.com/gainers');
+  await page.screenshot({
+    path: './screenshots/' + (new Date()).toLocaleString() + '.png',
+    fullPage: true
+  });
+  await browser.close();
+};
+
+new CronJob('31 06 * * 1-5', () => {
+
+  [0, 3, 5, 10, 20, 30, 60].forEach(min => {
+    setTimeout(async () => {
+      console.log(`scraping gainers for 6:31am + ${min} minutes`);
+      await scrapeGainers();
+    }, min * 1000 * 60);
+  });
+
+}, null, true);
